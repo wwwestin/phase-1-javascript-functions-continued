5fb4c42c369102ac3499796de141c1ee62aa5eca
Westin Humble <westin.h@gmail.com>
2022-01-15 11:30:09 -0500

    2nd attempt at passing last two tests-unsuccessful

--- index.js
+++ index.js
@@ -14,29 +14,15 @@ mondayWork();
 mondayWork("work from home");
 
 
-// Implement a function called wrapAdjective:
-
-// It should return a function
-// This "inner" function should:
-// take a single parameter that should default to "special". Name it however you wish.
-// return a String of the form "You are ..." where ... should be the adjective this function received wrapped in visual flair
-// It should take as parameter a String that will be used to create visual flair
-// You may call the parameter whatever you like, but its default value should be "*"
-// Call example: const encouragingPromptFunction = wrapAdjective("!!!")
-// Thus a total call should be:
-
-// wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%!"
-// Run learn to verify you've gotten this set of tests passing.
-
-function result() {
-    return "a hard worker";
+function result(emphatic = "*") {
+    let words = wrapAdjective();
+    let sentence = `You are ${emphatic}${words}${emphatic}!`;
+    return sentence; 
 }
 
-function wrapAdjective(emphatic = "*") {
-    let words = result()
-    let sentence = `You are ${emphatic}${words}${emphatic}!`;
-    console.log (sentence);
+function wrapAdjective() {
+    return "a hard worker";
 };
 
-wrapAdjective();
-wrapAdjective('||');
+result();
+result('||'); 
